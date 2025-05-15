2dcf69d5d06f34f91c398161f2895740827e4e06
Lauraine Delarosa <87099466+Lauraine18@users.noreply.github.com>
2025-05-14 21:59:44 -0400

    Update main.js

--- src/components/main.js
+++ src/components/main.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function Main() {
   return (
     <div className="bg-gradient-to-b from-dark-purple via-semi-dark-purple to-violet-500 font-mono">
-      <div className="bg-[url('./images/weather.png')] bg-no-repeat bg-center">
-        <div className=" h-screen flex flex-col items-center justify-end gap-3 p-10 text-3xl">
+      <div className="bg-[url('./images/weather.png')] h-screen bg-no-repeat bg-center">
+        <div className=" h-screen flex flex-col items-center justify-end gap-3 p-3 text-2xl">
           <h1 className="text-2xl text-white font-extrabold">Weather</h1>
           <p className="text-2xl text-yellow">Forecasts</p>
 
