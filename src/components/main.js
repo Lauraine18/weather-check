a99c3e5c180b75ee4d5a1f80ea222a688b520cfe
Lauraine Delarosa <87099466+Lauraine18@users.noreply.github.com>
2025-05-14 21:44:25 -0400

    Update main.js

--- src/components/main.js
+++ src/components/main.js
@@ -5,11 +5,11 @@ export default function Main() {
     <div className="bg-gradient-to-b from-dark-purple via-semi-dark-purple to-violet-500 font-mono">
       <div className="bg-[url('./images/weather.png')] h-screen bg-no-repeat bg-center">
         <div className=" h-screen flex flex-col items-center justify-end gap-3 p-10 text-3xl">
-          <h1 className="text-5xl text-white font-extrabold">Weather</h1>
-          <p className="text-5xl text-yellow">Forecasts</p>
+          <h1 className="text-2xl text-white font-extrabold">Weather</h1>
+          <p className="text-2xl text-yellow">Forecasts</p>
 
           <Link to="/forecast">
-            <button className="bg-yellow hover:shadow-2xl rounded-full px-8 py-2">
+            <button className="bg-yellow hover:shadow-2xl rounded-full px-6 py-2">
               Get Started
             </button>
           </Link>
